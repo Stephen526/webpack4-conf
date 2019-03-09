ebc901e976bca33cc378b6ff3df2f03ea1300ad2
miao <13545177350@126.com>
2019-03-09 10:24:48 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import obj from './a'
+import './index.css'
+import './style.less'
+document.getElementById('app').innerHTML=JSON.stringify(obj)
+document.getElementById('app').addEventListener('click',function(e){
+    e.target.className='active'
+})
+if(module.hot){
+    module.hot.accept()
+}
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
+const ExtractTextWebpackPlugin=require('extract-text-webpack-plugin')
+const cssExtract = new ExtractTextWebpackPlugin({
+    filename :'css/css.css',
+   // disable:true
+})
+const lessExtract = new ExtractTextWebpackPlugin({
+    filename :'css/less.css',
+    //disable:true
+})
+module.exports={
+    entry:'./src/index.js',
+    output:{
+        filename:'[name].[hash].js',
+        path:path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:cssExtract.extract({
+                    fallback:'style-loader',
+                    use:[
+                        {
+                            loader:'css-loader'
+                        }
+                    ]
+                })
+            },
+            {
+                test:/\.less$/,
+                use:lessExtract.extract({
+                    fallback:'style-loader',
+                    use:[
+                        {
+                            loader:'css-loader'
+                        },
+                        {
+                            loader:'less-loader'
+                        }
+                    ]
+                    
+                })
+                
+            }
+        ]
+    },
+    plugins:[
+        cssExtract,
+        lessExtract,
+        new CleanWebpackPlugin(['./dist']),
+        //打包html插件
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            template:'./src/index.html',
+            //hash:true,
+            minify:{
+                removeAttributeQuotes:true,
+                //collapseWhitespace:true
+            }
+          }),
+          new webpack.HotModuleReplacementPlugin()
+    ],
+    mode:'development',
+    resolve:{},
+    devServer:{
+        contentBase:'./dist',
+        port:3000,
+        compress:true,//服务端压缩
+        open:true,//自动打开浏览器
+        hot:true
+    }
+}
